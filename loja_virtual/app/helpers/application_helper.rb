43feda09a3baab13cce56ea7deb8d15861d76bb3
Guilherme O <guilherme_otran@hotmail.com>
2012-12-20 17:53:59 -0200

    CRUD Basica deve estar ok. Testar.

--- loja_virtual/app/helpers/application_helper.rb
+++ loja_virtual/app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  
+  def root_path
+    '/index'
+  end
+  
+  def user_signed_in?
+    false
+  end
+  
+  #logoff
+  def destroy_user_session_path
+    ''
+  end
+  
+  #create user
+  def new_user_session_path
+  end
+  
+  #user register edit.
+  # Checar se sera necessario
+  def edit_user_registration_path
+    ''
+  end
+  
+  #Novo usuario
+  def new_user_registration_path
+    ''
+  end
+  
+  
+  
 end
--- loja_virtual/config/routes.rb
+++ loja_virtual/config/routes.rb
@@ -1,4 +1,10 @@
 LojaVirtual::Application.routes.draw do
+  resources :products
+
+
+  resources :codes
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -11,7 +17,7 @@ LojaVirtual::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+     resources :products
 
   # Sample resource route with options:
   #   resources :products do
