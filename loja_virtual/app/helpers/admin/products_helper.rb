63c60cfe28a9b7c95184d0faecbaaf698008bdad
Guilherme <guilherme_otran@hotmail.com>
2013-01-07 01:41:16 -0200

    Admin product com todos os campos requeridos

--- loja_virtual/app/helpers/admin/products_helper.rb
+++ loja_virtual/app/helpers/admin/products_helper.rb
@@ -1,2 +1,8 @@
 module Admin::ProductsHelper
+
+  def form_select_categories
+      Category.all.map do |c|
+        [c.name, c.id]
+      end
+  end
 end
--- loja_virtual/app/models/product.rb
+++ loja_virtual/app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   
   # Mass-storange acessors
-  attr_accessible :category_id, :code, :description, :price, :image
+  attr_accessible :category_id, :code, :description, :price, :image, :category
 
   # Relations
   belongs_to :category
@@ -11,8 +11,8 @@ class Product < ActiveRecord::Base
   
   #Validations
   validates :code, uniqueness: true
-  validates :code, :price, :category, presence: true
-  validates :price, numericality: { only_float: true, greater_than: -0.0 }
+  validates :code, :description, :price, :category, presence: true
+  validates :price, numericality: { only_float: true, greater_than: 0 }
   validates :image, 
     allow_blank: true, 
     format: {
