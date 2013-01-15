8d0108117aab3ddf06924b9b34f4208f672259e1
Lucas Yuji Suguinoshita Aciole <lucas.aciole@gmail.com>
2013-01-15 17:22:32 -0200

    Adicionado Coffee e Admins na aplicação

--- loja_virtual/db/schema.rb
+++ loja_virtual/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130115122906) do
+ActiveRecord::Schema.define(:version => 20130115163239) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(:version => 20130115122906) do
   add_index "products", ["code"], :name => "index_products_on_code", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "name",                   :default => "", :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "name",                   :default => "",    :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -44,8 +44,9 @@ ActiveRecord::Schema.define(:version => 20130115122906) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
