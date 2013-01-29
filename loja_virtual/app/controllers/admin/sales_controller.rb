a4d02dcc2b21cbdf172ba542429ce27571a04dc3
Guilherme O <guilherme_otran@hotmail.com>
2013-01-29 15:05:50 -0200

    Index paginado

--- loja_virtual/app/controllers/admin/sales_controller.rb
+++ loja_virtual/app/controllers/admin/sales_controller.rb
@@ -2,10 +2,7 @@ class Admin::SalesController < ApplicationController
   def index
     @sales = Sale.scoped
     @sales = @sales.by_between_date(params[:search][:date_start], params[:search][:date_end]) if params[:search]
-    @sales = @sales.page(params[:page]).per(10)
-    respond_to do |f|
-      f.html
-    end
+    @sales = @sales.page(params[:page]).per(15)
   end
 
   def show
