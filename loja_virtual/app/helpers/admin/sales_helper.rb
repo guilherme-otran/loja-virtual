7fabfdc66c8a877f7d06b28b8f76710de5c49fbc
Monica Henriques Nallin <monica.nallin@gmail.com>
2013-01-29 17:41:35 -0200

    Detalhes da compra arrumados
    - Data da venda formatada para ficar mais simples;
    - Valor total do item adicionado
    - O valor total da venda está alinhado com a coluna do valor total dos itens.

--- loja_virtual/app/helpers/admin/sales_helper.rb
+++ loja_virtual/app/helpers/admin/sales_helper.rb
@@ -1,10 +1,6 @@
 module Admin::SalesHelper
 	def total_value(sales)
-		sales.inject(0) { |memo, sale| memo += sale.total_price }
-	end
-
-	def total_number(sales)
-		sales.inject(0) { |memo, sale| memo += 1 }
+		sales.inject(0) { |memo, sale| memo + sale.total_price }
 	end
 
 	def username(id)
@@ -25,5 +21,9 @@ module Admin::SalesHelper
 
 	def product(sale_item)
 		Product.find(sale_item.product_id)
+	end
+
+	def total_item_value (sale_item)
+		sale_item.product_price * sale_item.quantity
 	end
 end
