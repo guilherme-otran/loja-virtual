2351be38f71c42921e6dcc60e9230e58be723cc1
Lucas Yuji Suguinoshita Aciole <lucas.aciole@gmail.com>
2013-01-30 13:36:31 -0200

    Refatoração do método paginate_products
    Como algumas páginas da aplicação tem paginação diferente de outras, o método paginate_products agora recebe um parametro de páginas, para que possa ser utilizado por toda a aplicação.

--- loja_virtual/lib/product_filter_paginator.rb
+++ loja_virtual/lib/product_filter_paginator.rb
@@ -3,7 +3,7 @@ module ProductFilterPaginator
 		@products = (@products || Product).search(params)
 	end
 
-	def paginate_products
-		@products = (@products || Product).page(params[:page]).per(8)    
+	def paginate_products(quantity)
+		@products = (@products || Product).page(params[:page]).per(quantity)    
 	end
 end
