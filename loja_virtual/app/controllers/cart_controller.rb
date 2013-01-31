091d207ee501918f3b9f728c85afbdb7485ceab7
Lucas Yuji Suguinoshita Aciole <lucas.aciole@gmail.com>
2013-01-31 10:33:55 -0200

    Estruturação certa da árvore.
    Utilizei o merge pelo Gitx, que acabou não fazendo um merge correto na árvore. Embora as alterações da branch 'admin_category_filter' tenham entrado na master, o commit de merge não tem o ultimo commit da branch como parente.

--- loja_virtual/app/controllers/cart_controller.rb
+++ loja_virtual/app/controllers/cart_controller.rb
@@ -3,12 +3,12 @@ class CartController < ApplicationController
   include CartManager
   helper_method :cart
   
+  # GET /cart/show.html
+  # GET /cart/show.json    
   def show
     @items    = cart.items
     @products = @items.collect(&:product)
     
-    # GET /cart/show.html
-    # GET /cart/show.json    
     respond_to do |format|
       format.html { render layout: false  } 
       format.json { render json:   @items }
